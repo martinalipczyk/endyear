6dc30432582e6ba3cfc3a7974c2e0da9f103c848
yuto150 <yutotdp@gmail.com>
2024-05-29 12:49:22 -0400

    splitting text

---
+++ views/uploadnotes.js
@@ -0,0 +1,11 @@
+const userInput = prompt("Enter your notes in the format of 'Term1 : Definition1 ; Term2 : Definition2; etc.");
+const x = userInput.split(';');
+
+let map = new Map();
+
+for (let i = 0; i < x.length; i ++) {
+    var newArr = x[i].split(':');
+    map.set(newArr[0],newArr[1]);
+}
+
+console.log(map);
