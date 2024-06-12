f45a2c75e0ad65a60bc96957c1ff9f3e3fc425b0
martinalipczyk <marlip26@bergen.org>
2024-06-12 10:32:04 -0400

    working on connecting to database
    Co-authored-by: liamstrauser <liamstrauser@users.noreply.github.com>

--- public/todo.js
+++ public/todo.js
@@ -1,62 +1,141 @@
-document.querySelector('#push').onclick = function(){
-  if(document.querySelector('#newtask input').value.length == 0){
-      alert("please enter a task :)")
-  }
 
-  else{
-    console.log(document.querySelector('#newtask input').value)
-      document.querySelector('#tasks').innerHTML += `
-          <div class="task">
-              <span id="taskname">
-                  ${document.querySelector('#newtask input').value}
-              </span>
-              <button class="delete">
-                    <img src = "graphics/finishtaskwidget.png" width = 50 >
-                  <i class="far fa-trash-alt"></i>
-              </button>
-          </div>
-      `;
-      document.getElementById("textplace").value = "";
-      var current_tasks = document.querySelectorAll(".delete");
-      for(var i=0; i<current_tasks.length; i++){
-          current_tasks[i].onclick = function(){
-              this.parentNode.remove();
-          }
-      }
-  }
+document.addEventListener('DOMContentLoaded', fetchUserTasks);
+
+document.querySelector('#push').onclick = function () {
+    const taskName = document.querySelector('#newtask input').value;
+
+    if (taskName.length == 0) {
+        alert('Please enter a task :)');
+    } else {
+        console.log(taskName);
+
+        document.querySelector('#tasks').innerHTML += `
+            <div class="container">
+                <div class="row">
+                    <span class="col s3 left-align" id="taskname">${taskName}</span>
+                    <a href="#" class="col s2 btn right-align offset-s6 delete-task">
+                        <i class="far fa-trash-alt"></i>done
+                    </a>
+                    <p></p>
+                </div>
+            </div>
+        `;
+
+        document.getElementById("txt").value = "";
+
+        updateDeleteFunctionality();
+
+        addToDatabase(taskName);
+    }
+};
+
+function addToDatabase(taskName) {
+    fetch('/addTask', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ taskName: taskName }),
+    })
+        .then(response => response.json())
+        .then(data => console.log('Task added to the database:', data))
+        .catch(error => console.error('Error adding task to the database:', error));
+}
+
+function updateDeleteFunctionality() {
+    var current_tasks = document.querySelectorAll(".delete-task");
+
+    for (var i = 0; i < current_tasks.length; i++) {
+        current_tasks[i].onclick = function () {
+
+            var taskElement = this.parentNode;
+            var taskName = taskElement.querySelector("#taskname").innerText;
+
+
+            deleteFromDatabase(taskName);
+
+
+            taskElement.remove();
+        };
+    }
+}
+
+var input = document.querySelector('#newtask input');
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        if (input.value.length == 0) {
+            alert('Please enter a task :)');
+        } else {
+            console.log(input.value);
+            document.querySelector('#tasks').innerHTML += `
+                <div class="container">
+                    <div class="row">
+                        <span class="col s3 left-align" id="taskname">${input.value}</span>
+                        <a href="#" class="col s2 btn right-align offset-s6 delete-task">
+                            <i class="far fa-trash-alt"></i>done
+                        </a>
+                        <p></p>
+                    </div>
+                </div>
+            `;
+            input.value = "";
+            updateDeleteFunctionality();
+
+            addToDatabase(input.value);
+        }
+    }
+});
+
+
+function fetchUserTasks() {
+    fetch('/getUserTasks')
+        .then(response => response.json())
+        .then(data => {
+
+            displayTasks(data.tasks);
+        })
+        .catch(error => console.error('Error fetching user tasks:', error));
+}
+
+function displayTasks(tasks) {
+    const tasksContainer = document.querySelector('#tasks');
+
+
+    tasksContainer.innerHTML = '';
+
+
+    tasks.forEach(task => {
+        tasksContainer.innerHTML += `
+            <div class="container">
+                <div class="row">
+                    <span class="col s3 left-align" id="taskname">${task.task_name}</span>
+                    <a href="#" class="col s2 btn right-align offset-s6 delete-task">
+                        <i class="far fa-trash-alt"></i>done
+                    </a>
+                    <p></p>
+                </div>
+            </div>
+        `;
+    });
+
+
+    updateDeleteFunctionality();
 }
 
 
-var input = document.querySelector('#newtask input');
-
-input.addEventListener("keypress", function(event) {
-
-  if (event.key === "Enter") {
-
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("please enter a task :)")
-    }
-  
-    else{
-      console.log(document.querySelector('#newtask input').value)
-        document.querySelector('#tasks').innerHTML += `
-            <div class="task">
-                <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
-                </span>
-                <button class="delete">
-                      <img src = "graphics/finishtaskwidget.png" width = 50 >
-                    <i class="far fa-trash-alt"></i>
-                </button>
-            </div>
-        `;
-        document.getElementById("textplace").value = "";
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-    }
-  }
-});
+function deleteFromDatabase(taskName) {
+    
+    fetch('/deleteTask', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ taskName: taskName }),
+    })
+    .then(response => {
+        console.log(response); 
+        return response.json();
+    })
+    .then(data => console.log('Task deleted from the database:', data))
+    .catch(error => console.error('Error deleting task from the database:', error));
+}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const db = require('./db/db_connection.js');
 const app = express();
 const port = 3000 || process.env.PORT;
 const server = http.createServer(app);
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.static("views"));
@@ -22,6 +23,9 @@ app.get("/", (req, res) => {
 
 app.get("/databases", (req, res) => {
     res.render("databases", {user_id: userid, username: userna});
+})
+app.get("/index", (req, res) => {
+    res.render("index", {user_id: userid, username: userna});
 })
 
 app.get("/gc", (req, res) => {
@@ -73,5 +77,111 @@ app.get("/createaccount", (req, res) => {
 })
 
 
+app.post('/register', (req, res) => {
+    const {first_name, last_name, username, password, email} = req.body;
+
+    const query = 'INSERT INTO user (first_name, last_name, username, password, email) VALUES (?, ?, ?, ?, ?)';
+    db.query(query, [first_name, last_name, username, password, email], (err, results) => {
+        if (err) {
+            console.error('Error registering user: ' + err.message);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        db.query('SELECT user_id, username FROM user WHERE username = ? AND password = ?', [username, password], (err, results) => {
+            if (err) {
+                console.error('Error executing login query: ' + err.message);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
+
+            if (results.length === 1) {
+                userid = results[0].user_id;
+                userna = results[0].username;
+                res.render('index', { user_id: userid, username: userna });
+            } else {
+                res.render('login');
+            }
+        });
+    });
+});
 
 
+
+  
+app.post('/dologin', (req, res) => {
+    const { username, password } = req.body;
+  
+    const query = 'SELECT user_id, username FROM user WHERE username = ? AND password = ?';
+    db.query(query, [username, password], (err, results) => {
+      if (err) {
+        console.error('Error executing query: ' + err.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+      if (results.length === 1) {
+        userid = results[0].user_id;
+        userna = results[0].username;
+        res.render('index', {user_id: userid, username: userna});
+      } else {
+        res.render("login");
+      }
+    });
+});
+
+app.post('/addTask', (req, res) => {
+    const { taskName } = req.body;
+
+    if (!taskName || taskName.trim() === '') {
+        return res.status(400).json({ error: 'Task name cannot be empty' });
+    }
+
+    console.log('Received taskName:', taskName);
+    const insertTaskQuery = 'INSERT INTO tasks (user_id, task_name) VALUES (?, ?)';
+    db.query(insertTaskQuery, [userid, taskName], (insertErr, insertResults) => {
+        if (insertErr) {
+            console.error('Error saving task: ' + insertErr.message);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        console.log('Received taskName:', taskName);
+        res.status(200).json({ message: 'Task saved successfully' });
+    });
+});
+
+app.post('/deleteTask', (req, res) => {
+    const { taskName } = req.body; 
+
+    if (!userid) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const deleteTaskQuery = 'DELETE FROM tasks WHERE task_name = ? AND user_id = ?';
+    db.query(deleteTaskQuery, [taskName, userid], (deleteErr, deleteResults) => {
+        if (deleteErr) {
+            console.error('Error deleting task: ' + deleteErr.message);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        if (deleteResults.affectedRows > 0) {
+            res.json({ message: 'Task deleted successfully' });
+        } else {
+            res.status(404).json({ error: 'Task not found' });
+        }
+    });
+});
+
+app.get('/getUserTasks', (req, res) => {
+
+    if (!userid) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+
+    const getUserTasksQuery = 'SELECT task_name FROM tasks WHERE user_id = ?';
+    db.query(getUserTasksQuery, [userid], (err, results) => {
+        if (err) {
+            console.error('Error fetching user tasks:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        const tasks = results.map(result => ({ task_name: result.task_name }));
+
+        res.json({ tasks: tasks });
+    });
+});
