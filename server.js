54d4203c4063988deb88c02b9f10b8c48d44566f
liamstrauser <liamstrauser@gmail.com>
2024-06-11 23:13:05 -0400

    reorganizing

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const socket = require("socket.io");
+// const socket = require("socket.io");
 const http = require("http");
 const axios = require("axios");
 
@@ -9,75 +9,75 @@ const server = http.createServer(app);
 
 app.use(express.static("views"));
 
-const io = socket(server);
+// const io = socket(server);
 
 server.listen(port, () => console.log(`App server listening on ${port}. (Go to http://localhost:${port})`));
 
-let players = [];
-let index = 0;
-let questions = [];
-let scores = {};
+// let players = [];
+// let index = 0;
+// let questions = [];
+// let scores = {};
 
-const getQuestions = async (amount) => {
-    try {
-        const response = await axios.get(`https://opentdb.com/api.php?amount=${amount}&type=multiple`);
-        questions = response.data.results;
-    } catch (error) {
-        console.error("error fetching questions: ", error);
-    }
-};
+// const getQuestions = async (amount) => {
+//     try {
+//         const response = await axios.get(`https://opentdb.com/api.php?amount=${amount}&type=multiple`);
+//         questions = response.data.results;
+//     } catch (error) {
+//         console.error("error fetching questions: ", error);
+//     }
+// };
 
-io.on("connection", (socket) => {
-    console.log("Made socket connection", socket.id);
-    players.push(socket.id);
-    scores[socket.id] = { correct: 0, wrong: 0 };
+// io.on("connection", (socket) => {
+//     console.log("Made socket connection", socket.id);
+//     players.push(socket.id);
+//     scores[socket.id] = { correct: 0, wrong: 0 };
 
-    socket.emit('serverToClient', 'test: hello client');
+//     socket.emit('serverToClient', 'test: hello client');
 
-    socket.on('clientToServer', (data) => {
-        console.log(data);
-    });
+//     socket.on('clientToServer', (data) => {
+//         console.log(data);
+//     });
 
-    socket.on("selectQuestions", async (amount) => {
-        await getQuestions(amount);
-        if (players.length === 2) {
-            startGame();
-        }
-    });
+//     socket.on("selectQuestions", async (amount) => {
+//         await getQuestions(amount);
+//         if (players.length === 2) {
+//             startGame();
+//         }
+//     });
 
-    socket.on("disconnect", () => {
-        players = players.filter(player => player !== socket.id);
-        delete scores[socket.id];
-        console.log("Player disconnected", socket.id);
-    });
+//     socket.on("disconnect", () => {
+//         players = players.filter(player => player !== socket.id);
+//         delete scores[socket.id];
+//         console.log("Player disconnected", socket.id);
+//     });
 
-    socket.on("submitAnswer", (answer) => {
-        if (questions[index].correct_answer === answer) {
-            scores[socket.id].correct++;
-            io.emit("updateScores", scores);
-            io.emit("correctAnswer", scores[socket.id]);
-        } else {
-            scores[socket.id].wrong++;
-            io.emit("updateScores", scores);
-            io.emit("wrongAnswer", scores[socket.id]);
-        }
-        nextQuestion();
-    });
-});
+//     socket.on("submitAnswer", (answer) => {
+//         if (questions[index].correct_answer === answer) {
+//             scores[socket.id].correct++;
+//             io.emit("updateScores", scores);
+//             io.emit("correctAnswer", scores[socket.id]);
+//         } else {
+//             scores[socket.id].wrong++;
+//             io.emit("updateScores", scores);
+//             io.emit("wrongAnswer", scores[socket.id]);
+//         }
+//         nextQuestion();
+//     });
+// });
 
-const startGame = () => {
-    index = 0;
-    io.emit("startGame", questions[index]);
-};
+// const startGame = () => {
+//     index = 0;
+//     io.emit("startGame", questions[index]);
+// };
 
-const nextQuestion = () => {
-    index++;
-    if (index < questions.length) {
-        io.emit("newQuestion", questions[index]);
-    } else {
-        io.emit("gameOver");
-    }
-};
+// const nextQuestion = () => {
+//     index++;
+//     if (index < questions.length) {
+//         io.emit("newQuestion", questions[index]);
+//     } else {
+//         io.emit("gameOver");
+//     }
+// };
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/multitrivia.html");
